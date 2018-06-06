3d4a831b5a7452e561cc9b9e3594c844988ce7c1
Вячеслав <slavik@itservices.in.ua>
2018-06-06 17:37:49 +0300

    fix

--- var/space/root/core/collection/arra/collection.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-return [["path" => "collection/root/core/arra", "name" => 'This is core item', "value" => Core\Space::class, "bundle" => ["file" => 'App/root/NativePhpEditor/SpaceBundle.php', "position" => 2], "enabled" => true]];
--- var/space/root/core/key/brb/variations.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-return [["path" => "key/root/core/brb", "name" => 'This is core item', "value" => Core\Space::class, "bundle" => ["file" => 'App/root/NativePhpEditor/SpaceBundle.php', "position" => 0]]];
