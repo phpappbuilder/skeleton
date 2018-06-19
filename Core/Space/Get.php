59cde705b9daa4b82b679f81d80240239d5edd75
Вячеслав <slavik@itservices.in.ua>
2018-06-19 17:26:02 +0300

    Now cli cmd use SpaceCollection root/core/cli

--- Core/Space/Get.php
+++ Core/Space/Get.php
@@ -8,7 +8,7 @@ class Get
     public static $temp = 'var/space';
 
     //Возвращает значение ключа
-    static function GetKey( $path ) {
+    static function Key( $path ) {
         $space = explode ("/",$path);
         if (is_file(self::$temp.'/'.$space['0'].'/'.$space['1'].'/key/'.$space['2'].'/value.php')) {
             return require (self::$temp.'/'.$space['0'].'/'.$space['1'].'/key/'.$space['2'].'/value.php');
@@ -17,7 +17,7 @@ class Get
     }
 
     //возвращает коллекцию
-    static function GetCollection( $path ) {
+    static function Collection( $path ) {
         $space = explode ("/",$path);
         if (is_file(self::$temp.'/'.$space['0'].'/'.$space['1'].'/collection/'.$space['2'].'/return.php')) {
             return require (self::$temp.'/'.$space['0'].'/'.$space['1'].'/collection/'.$space['2'].'/return.php');
